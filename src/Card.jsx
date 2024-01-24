f8d27d6cb180206374060d859390f7265f48a1c2
Sania <121291138+Sania90503@users.noreply.github.com>
2024-01-24 17:26:23 +0530

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,14 +3,23 @@ import Star from './Star.png'
 
 
 export default function Card(props){
-    
+   
+    let badgeText ; 
+    if(props.openSpots===0){
+        badgeText = "SOLD OUT"
+    }
+    else if (props.location === "Online"){
+        badgeText="ONLINE"
+    }
+
     return(
         <>
-        <div className="card">
-           <img src= {`./${props.img}`} alt="Card-Image" className='card--image' /> 
+        <div className="card">           
+           {badgeText && <div className="card--badge">{badgeText}</div>}
+           <img src= {`./${props.coverImg}`} alt="Card-Image" className='card--image' /> 
            <div className="card--status">
                     <img src={Star} alt="Star" className='star' />
-            <span> {props.rating} </span><span className='gray'> ({props.reviewCount}) •  </span><span className='gray'>USA</span>
+            <span> {props.stats.rating} </span><span className='gray'> ({props.stats.reviewCount}) •  </span><span className='gray'>USA</span>
             <p className='card--title'>{props.title}</p>
             <p className='card--price'> <b>From ${props.price} /</b> Person </p>
             </div>
